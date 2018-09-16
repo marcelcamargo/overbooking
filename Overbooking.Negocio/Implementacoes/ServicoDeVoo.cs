b3fd32f37dd398a480bcd247170ff64f4a851535
Marcel <marcel_bcamargo_12@hotmail.com>
2018-09-16 13:01:45 -0300

    Adicionando validações

--- Overbooking.Negocio/Implementacoes/ServicoDeVoo.cs
+++ Overbooking.Negocio/Implementacoes/ServicoDeVoo.cs
@@ -2,6 +2,7 @@
 using Overbooking.Compartilhado.Interfaces;
 using Overbooking.Dados.Interfaces;
 using Overbooking.Negocio.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -9,6 +10,8 @@ namespace Overbooking.Negocio.Implementacoes
 {
     internal class ServicoDeVoo : IServicoDeVoo
     {
+        private const int CAPACIDADE_MAXIMA_PADRAO = 200;
+
         private IRepositorio<IPassageiroVoo> _repositorio;
 
         public ServicoDeVoo(IRepositorio<IPassageiroVoo> repositorio)
@@ -18,6 +21,13 @@ namespace Overbooking.Negocio.Implementacoes
 
         public void AdicionePassageiro(IPassageiroVoo passageiroVoo)
         {
+            var qtdPassageirosNoVoo = ObtenhaQtdPassageirosNoVoo(passageiroVoo.Rota, passageiroVoo.DataDeSaida);
+
+            if (qtdPassageirosNoVoo >= CAPACIDADE_MAXIMA_PADRAO)
+            {
+                throw new Exception($"A capacidade máxima de {CAPACIDADE_MAXIMA_PADRAO} passageiros do vôo [{passageiroVoo.Rota} as {passageiroVoo.DataDeSaida}] foi atingida.");
+            }
+
             _repositorio.Adicione(passageiroVoo);
         }
 
@@ -46,6 +56,22 @@ namespace Overbooking.Negocio.Implementacoes
             voo.RiscoDeOverbooking = CalculadoraDeRisco.CalculeProbabilidadeDeComparecimento(voo);
 
             return voo;
+        }
+
+        private int ObtenhaQtdPassageirosNoVoo(IRota rota, IDataDeSaida data)
+        {
+            const int NENHUM_PASSAGEIRO = 0;
+
+            var passageirosAgrupadosPorVoo = ObtenhaTodosPassageiros().GroupBy(x => new { x.Rota.Origem, x.Rota.Destino, x.DataDeSaida.Data })
+                                            .Where(x => x.Key.Origem == rota.Origem &&
+                                                        x.Key.Destino == rota.Destino &&
+                                                        x.Key.Data == data.Data)
+                                            .Select(group => new
+                                            {
+                                                qtdPassageiros = group.Count()
+                                            });
+
+            return passageirosAgrupadosPorVoo.FirstOrDefault()?.qtdPassageiros ?? NENHUM_PASSAGEIRO;
         }
     }
 }
